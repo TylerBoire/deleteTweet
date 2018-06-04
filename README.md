0192cb1a973a7c75fd8f82740292f99d81c6fde9
TylerBoire <tylerboire@gmail.com>
2018-06-04 19:49:13 -0400

    Init push

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # deleteTweet
+
+This is a tool used to delete past Tweets, Likes, and DMs. 
+
+You will have to create an API key from https://apps.twitter.com/ with Direct Messages permisions
+if that is a feature you want to use.
+
+You will need to perminantly turn off DebugMode or specify in the CLI options. 
+
+Usage:
+
+python deleteTweet.py [Options]
+"-t", "--tweets", help="Delete the Tweets", action='store_true'
+"-l", "--likes", help="Delete your likes", action='store_true'
+"-d", "--DM", help="Delete da DMs", action='store_true'
+"-k", "--keep", help="How many days to keep. Default 2", default=2
+"-b" "--debug", help="Turn off debug mode", action='store_false'
---
+++ deleteTweet.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import tweepy
+from datetime import datetime, timedelta
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--tweets", help="Delete the Tweets", action='store_true')
+parser.add_argument("-l", "--likes", help="Delete your likes", action='store_true')
+parser.add_argument("-d", "--DM", help="Delete da DMs", action='store_true')
+parser.add_argument("-k", "--keep", help="How many days to keep. Default 2", default=2)
+parser.add_argument("-b" "--debug", help="Turn off debug mode", action='store_false')
+args = parser.parse_args()
+
+# Load your keys from https://apps.twitter.com/
+# You must give the app permision to edit DMs if you want that functionality. 
+consumer_key = ""
+consumer_secret = ""
+access_key = ""
+access_secret = ""
+
+# Set on command line, or change permanently or shit wont work 
+debugMode = True
+
+days_to_keep = args.keep
+cutoff_date = datetime.utcnow() - timedelta(days=days_to_keep)
+
+def oauthLogin(consumer_key, consumer_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth_url = auth.get_authorization_url()
+    
+    verify_code = raw_input("Authenticate at %s and then enter you verification code here > " % auth_url) 
+    auth.get_access_token(verify_code)
+    
+    return tweepy.API(auth)
+
+def deleteStatus(api):
+    for status in tweepy.Cursor(api.user_timeline).items():
+        if status.id and status.created_at < cutoff_date:
+            if debugMode:
+                print status.created_at
+            else:
+		        api.destroy_status(status.id)
+
+def deleteLikes(api):	
+    for liked in tweepy.Cursor(api.favorites).items():
+        if liked.created_at < cutoff_date:
+            if debugMode:
+                print liked.created_at
+            else:
+		        api.destroy_favorite(liked.id_str)
+    
+def deleteDM(api):
+    for DM in tweepy.Cursor(api.sent_direct_messages).items():
+        if DM.created_at < cutoff_date:
+            if debugMode:
+                print DM.created_at
+            else:
+                api.destroy_direct_message(DM.id)
+
+if __name__ == "__main__":
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    print "Authenticated as: %s" % api.me().screen_name
+    if args.tweets:
+        deleteStatus(api)
+    if args.likes:
+        deleteLikes(api)
+    if args.DM:
+        deleteDM(api)
